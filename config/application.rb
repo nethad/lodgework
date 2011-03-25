74ff3bb295527cefb0c8f8b36cf0d65d045b6be9
Thomas Gerig <thomas.gerig@gmail.com>
2011-03-25 13:11:42 +0100

    Rails Model generiert

--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,4 @@
 require File.expand_path('../boot', __FILE__)
-
 require 'rails/all'
 
 # If you have a Gemfile, require the gems listed there, including any gems
@@ -38,5 +37,6 @@ module Lodgework
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
+    config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   end
 end
