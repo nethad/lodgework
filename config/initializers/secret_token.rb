6b1c4b95897d1a170beac233392a58eb36d136e7
Thomas Gerig <thomas.gerig@gmail.com>
2011-03-25 12:24:41 +0100

    Erster Commit. Rails Projekt generiert.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lodgework::Application.config.secret_token = '1e6819ea82e5c5d2c0a1bf6c0b4c3933d5eef0d5118389e2c6d1c00b4b605f769c6a974453b64dfd611485549a062d29c8dfcd0d846751056bf5179f8268a99b'
